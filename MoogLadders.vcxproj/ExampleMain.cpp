c2046b24b82e931533cef0cfe46d7a4ca55ac6bb
morgan <mwcmitchell@gmail.com>
2021-09-08 07:52:05 -0700

    remove unused models and other files

--- MoogLadders.vcxproj/ExampleMain.cpp
+++ MoogLadders.vcxproj/ExampleMain.cpp
@@ -1,42 +1,21 @@
 #if defined(_MSC_VER)
     #pragma comment(lib, "dsound.lib")
 #endif
+
 #include <sstream>
-
-#include "AudioDevice.h"
-#include "NoiseGenerator.h"
 #include <iostream>
-#include <sndfile.h>
-#include "sndfile.hh"
-#include "StilsonModel.h"
-#include "OberheimVariationModel.h"
-#include "SimplifiedModel.h"
-#include "ImprovedModel.h"
-#include "HuovilainenModel.h"
-#include "KrajeskiModel.h"
-#include "RKSimulationModel.h"
-#include "MicrotrackerModel.h"
-#include "MusicDSPModel.h"
-#include "RKSimulationModel.h"
-
-
 #include <thread>
 #include <chrono>
 #include <vector>
 
-int main()
-{
-	AudioDevice::ListAudioDevices();
-	
-	//int desiredSampleRate = 44100;
+#include <sndfile.h>
+#include "sndfile.hh"
+#include "ImprovedModel.h"
 
 
-	#define NUM_CHANNELS 2
-	#define SAMPLE_RATE 26040
-	int CUTOFF = 1500;
-	float RESONANCE = 2.1f;
-	
-	std::string infile_name = "C:/Users/mwcm/Documents/GitHub/pitcher/rain4.wav";
+int main(int cutoff = 1500, int resonance = 0.1f, int sample_rate = 26040, int num_channels = 2)
+{	
+	std::string infile_name = "C:/Users/mwcm/Documents/GitHub/pitcher/808.wav";
 
 	// Open input file.
 	SndfileHandle infile_handle(infile_name);
@@ -46,7 +25,7 @@ int main()
 		return 1;
 	}
 
-	// Show file stats
+	// file stats
 	int64_t in_frames = infile_handle.frames();
 	int in_channels = infile_handle.channels();
 	int in_samplerate = infile_handle.samplerate();
@@ -55,45 +34,17 @@ int main()
 	std::vector<float> in_data(in_frames * in_channels);
 	infile_handle.read(in_data.data(), in_data.size());
 
-
-	AudioDevice device(2, SAMPLE_RATE);
-	device.Open(device.info.id);
-
-	ImprovedMoog improvedModel(SAMPLE_RATE);
-	improvedModel.SetCutoff(CUTOFF);
-	improvedModel.SetResonance(RESONANCE);
+	ImprovedMoog improvedModel(sample_rate);
+	improvedModel.SetCutoff(cutoff);
+	improvedModel.SetResonance(resonance);
 	improvedModel.Process(in_data.data(), in_data.size());
 
-	//StilsonMoog stilsonModel(SAMPLE_RATE);
-	//stilsonModel.Process(in_data.data(), in_data.size());
-	
-	SimplifiedMoog simplifiedModel(SAMPLE_RATE);
-	//simplifiedModel.Process(in_data.data(), in_data.size());
-	
-	HuovilainenMoog huovilainenModel(SAMPLE_RATE);
-	//huovilainenModel.Process(in_data.data(), in_data.size());
-
-	MicrotrackerMoog microtrackerModel(SAMPLE_RATE);
-	//microtrackerModel.Process(in_data.data(), in_data.size());
-
-	MusicDSPMoog musicdspModel(SAMPLE_RATE);
-	//musicdspModel.Process(in_data.data(), in_data.size());
-	
-    KrajeskiMoog aaronModel(SAMPLE_RATE);
-	//aaronModel.Process(in_data.data(), in_data.size());
-
-	RKSimulationMoog rkModel(SAMPLE_RATE);
-	//rkModel.Process(in_data.data(), in_data.size());
-	
-	OberheimVariationMoog oberheimModel(SAMPLE_RATE);
-	//oberheimModel.Process(in_data.data(), in_data.size());
-	
-	const int len = infile_handle.frames() / SAMPLE_RATE;
+	const int len = infile_handle.frames() / sample_rate;
 
 	SF_INFO sfinfo;
 
 	std::ostringstream oss;
-	oss << "C:/Users/mwcm/Documents/GitHub/pitcher/rain4_cutoff_" << CUTOFF << "_resonance_" << RESONANCE << ".WAV";
+	oss << "C:/Users/mwcm/Documents/GitHub/pitcher/808_cutoff_" << cutoff << "_resonance_" << resonance << ".WAV";
 	std::string aeiou = oss.str();
 	const char* filename = aeiou.c_str();
 
@@ -108,9 +59,6 @@ int main()
 	const char* error = sf_strerror(outfile);
 	std::cout << error << "\n";
 	sf_close(outfile);
-	// file = SndfileHandle("C:/Users/mwcm/Documents/GitHub/pitcher/warning6ud1_FILTERED.wav", SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_16, 2, SAMPLE_RATE);
-	
-	// device.Play(in_data);
 	
 	return 0;
 }
